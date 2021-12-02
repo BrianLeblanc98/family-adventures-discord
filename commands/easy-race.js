62d455606c7bcb0ec6c32042d16bc4e84a044b3c
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 18:17:45 -0500

    update descriptions

--- commands/easy-race.js
+++ commands/easy-race.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('easy-race')
-		.setDescription('Make our family proud, and take out some poser who thinks their car is fast. Bet however much you want, and you\'ll get 10% as prize winnings!')
+		.setDescription('Make our family proud, and take out some poser who thinks their car is fast.')
         .addIntegerOption(option => 
             option.setName('bet')
-                .setDescription('How much you want to bet')
+                .setDescription('How much you want to bet, you\'ll get 10% of what you bet as winnings.')
                 .setRequired(true)),
 	async execute(interaction) {
         if (!fs.existsSync(`./data/users/${interaction.user.id}.json`)){
