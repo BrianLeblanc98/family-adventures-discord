2ab9e8b80250d8171ee385fafb94ca0720363a2a
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 17:51:04 -0500

    return instead of using an else block

--- commands/join.js
+++ commands/join.js
@@ -8,24 +8,25 @@ module.exports = {
 	async execute(interaction) {
         if (fs.existsSync(`./data/users/${interaction.user.id}.json`)){
             await interaction.reply(`You're already part of the family <@${interaction.user.id}>!`);
-        } else {
-            newUser = {
-                "id": interaction.user.id,
-                "name": interaction.user.name,
-                "bal": 0,
-                "current_car_id": "000",
-                "cars": [],
-                "items": [],
-                "mods": []
-            }
-            
-            var jsonString = JSON.stringify(newUser);
-    
-            // Probably want to async this at some point
-            fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
-            console.log(`${interaction.user.id}.json created`);
-    
-            await interaction.reply(`Welcome to the family <@${interaction.user.id}>! You\'ll need a car before you can get yourself some Coronas, which one do you wanna start with?`);
+            return;
         }
+        newUser = {
+            "id": interaction.user.id,
+            "name": interaction.user.name,
+            "bal": 0,
+            "current_car_id": "000",
+            "cars": [],
+            "items": [],
+            "mods": []
+        }
+        
+        var jsonString = JSON.stringify(newUser);
+
+        // Probably want to async this at some point
+        fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
+        console.log(`${interaction.user.id}.json created`);
+
+        await interaction.reply(`Welcome to the family <@${interaction.user.id}>! You\'ll need a car before you can get yourself some Coronas, which one do you wanna start with?`);
+
 	},
 };
