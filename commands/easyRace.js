20e1acdac834001d7484e307dbd351b22bcd0643
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 18:15:32 -0500

    Previous commit didn't add 'bet'; Add 'easyRace' command

---
+++ commands/easyRace.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('easyRace')
+		.setDescription('Make our family proud, and take out some poser who thinks their car is fast. Bet however much you want, and you\'ll get 10% as prize winnings!')
+        .addIntegerOption(option => 
+            option.setName('bet')
+                .setDescription('How much you want to bet')
+                .setRequired(true)),
+	async execute(interaction) {
+        if (!fs.existsSync(`./data/users/${interaction.user.id}.json`)){
+            await interaction.reply(`You're not part of the family <@${interaction.user.id}>! Join us by using /join`);
+            return;
+        }
+
+        let bet = interaction.options.getInteger('bet');
+        
+		await interaction.reply(`bet was ${bet}`);
+	},
+};
