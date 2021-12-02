0e697dc019e56d6924a5baf1c4a3536942a37603
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 18:27:02 -0500

    complete easy-race functionality

--- commands/easy-race.js
+++ commands/easy-race.js
@@ -16,7 +16,29 @@ module.exports = {
         }
 
         let bet = interaction.options.getInteger('bet');
-        
+        let userJson = JSON.parse(fs.readFileSync(`./data/users/${interaction.user.id}.json`));
+
+        if (bet > userJson.bal) {
+            await interaction.reply(`Your current balance is ${userJson.bal}, don't try to lie to the family about how much you have!`);
+            return;
+        }
+
+        if (Math.random() < 0.9) {
+            // WIN
+            let winnings = Math.floor(bet * 0.1);
+            userJson.bal += winnings;
+
+            await interaction(`You won for the family! Congrats on your ${winnings} Corona win!`);
+        } else {
+            userJson.bal -= bet;
+            await interaction(`You lost it for the family. You lose your ${bet} bet `);
+        }
+
+        let jsonString = JSON.stringify(userJson);
+
+        // Probably want to async this at some point
+        fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
+
 		await interaction.reply(`bet was ${bet}`);
 	},
 };
