c0350bb24eee1bd124e0e759df03075dc5d80864
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 17:40:55 -0500

    store users as json file when they join

--- commands/join.js
+++ commands/join.js
@@ -1,11 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require(fs);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('join')
 		.setDescription('Join the family, get your first car, and get ready to race!'),
 	async execute(interaction) {
+        newUser = {
+            "id": interaction.user.id,
+            "name": interaction.user.name,
+            "bal": 0,
+            "current_car_id": "000",
+            "cars": [],
+            "items": [],
+            "mods": []
+        }
+        
+        var jsonString = JSON.stringify(newUser);
+        await fs.writeFile(`./data/users/${interaction.user.id}.json`, jsonString);
+        console.log(`${interaction.user.id}.json created`);
+
 		await interaction.reply(`Welcome to the family <@${interaction.user.id}>! You\'ll need a car before you can get yourself some Coronas, which one do you wanna start with?`);
-        console.log(interaction.user.id); // store this somewhere eventually
 	},
 };
