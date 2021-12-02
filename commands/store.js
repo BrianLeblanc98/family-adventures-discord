66e4edf57219ed851b74693667513a1e7970edbd
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-02 16:25:33 -0500

    add base for store

---
+++ commands/store.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, Message } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('store')
+		.setDescription('Look at and buy many things from the family store!'),
+	async execute(interaction) {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('cars')
+                    .setLabel('Cars')
+                    .setStyle('PRIMARY'),
+                new MessageButton()
+                    .setCustomId('mods')
+                    .setLabel('Mods')
+                    .setStyle('PRIMARY'),
+                new MessageButton()
+                    .setCustomId('items')
+                    .setLabel('Items')
+                    .setStyle('PRIMARY')
+            );
+
+		await interaction.reply({ content: "What do you want to shop for?", components: [row]});
+	},
+};
