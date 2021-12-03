277961b8cba1d147b90c8cd7d4452be49113df15
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 00:52:57 -0500

    finish join command for now

--- commands/join.js
+++ commands/join.js
@@ -6,31 +6,25 @@ module.exports = {
 		.setName('join')
 		.setDescription('Join the family, get your first car, and get ready to race!'),
 	async execute(interaction) {
-        // if (fs.existsSync(`./data/users/${interaction.user.id}.json`)){
-        //     await interaction.reply(`You're already part of the family <@${interaction.user.id}>!`);
-        //     return;
-        // }
         if (await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne({'id': interaction.user.id.toString()})){
             await interaction.reply(`You're already part of the family <@${interaction.user.id}>!`);
             return;
         }
+
         let newUser = {
             "id": interaction.user.id,
-            "name": interaction.user.name,
+            "name": interaction.user.username,
             "bal": 0,
             "current_car_id": "000",
             "cars": [],
             "items": [],
             "mods": []
         }
+        
         await mongoClient.db('familyAdventuresDiscordDb').collection('users').insertOne(newUser)
-        let jsonString = JSON.stringify(newUser);
+        console.log(`${interaction.user.username} added in db`);
 
-        // Probably want to async this at some point
-        fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
-        console.log(`${interaction.user.id}.json created`);
-
-        await interaction.reply(`Welcome to the family <@${interaction.user.id}>! You\'ll need a car before you can get yourself some Coronas, which one do you wanna start with?`);
+        await interaction.reply(`Welcome to the family <@${interaction.user.id}>! Start doing some /work, then you can move onto /easy-race!`);
 
 	},
 };
