3503f55e4fc2195dfa314c6893c049625995f91f
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 02:24:22 -0500

    update util script

--- util/add-starter-cars-to-db.js
+++ util/add-starter-cars-to-db.js
@@ -1,9 +1,15 @@
+const { MongoClient } = require('mongodb');
+const { mongoUri } = require('./config.json');
+
+mongoClient = new MongoClient(mongoUri);
+
 const a = require('../data/store/cars/325i.json');
 const b = require('../data/store/cars/civic.json');
 const c = require('../data/store/cars/mustang.json');
 
 async function run() {
     try {
+        await mongoClient.connect();
         await mongoClient.db('familyAdventuresDiscordDb').collection('cars').insertOne(a);
         await mongoClient.db('familyAdventuresDiscordDb').collection('cars').insertOne(b);
         await mongoClient.db('familyAdventuresDiscordDb').collection('cars').insertOne(c);
