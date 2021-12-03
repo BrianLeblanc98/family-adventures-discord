e70f8abaf8de84d1f1197e88b2f03ba808dbc306
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 01:13:52 -0500

    update work command to use db

--- commands/bal.js
+++ commands/bal.js
@@ -6,13 +6,12 @@ module.exports = {
 		.setName('bal')
 		.setDescription('Shows your cuurent balance of Coronas, the currency in this family.'),
 	async execute(interaction) {
-        if (!await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne({'id': interaction.user.id.toString()})){
+        let userData = await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne({'id': interaction.user.id.toString()})
+        if (userData){
             await interaction.reply(`You're not part of the family <@${interaction.user.id}>! Join us by using /join`);
             return;
         }
 
-        let userData = await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne({'id': interaction.user.id.toString()})
-        console.log(userData.bal);
         await interaction.reply(`Your current balance is ${userData.bal} Coronas`);
 	},
 };
--- commands/work.js
+++ commands/work.js
@@ -6,19 +6,25 @@ module.exports = {
 		.setName('work')
 		.setDescription('Do some small work for the family, and get a small amount of Corona in return'),
 	async execute(interaction) {
-        if (!fs.existsSync(`./data/users/${interaction.user.id}.json`)){
+        let query = {'id': interaction.user.id.toString()};
+        let userData = await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne(query);
+        if (userData){
             await interaction.reply(`You're not part of the family <@${interaction.user.id}>! Join us by using /join`);
             return;
         }
-        
-        let userJson = JSON.parse(fs.readFileSync(`./data/users/${interaction.user.id}.json`));
-        userJson.bal += 2;
 
-        let jsonString = JSON.stringify(userJson);
+        let newBal = userData.bal + 2;
+        let update = { $set: { 'bal': newBal } }
+        await mongoClient.db('familyAdventuresDiscordDb').collection('users').updateOne(query, update)
 
-        // Probably want to async this at some point
-        fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
+        // let userJson = JSON.parse(fs.readFileSync(`./data/users/${interaction.user.id}.json`));
+        // userJson.bal += 2;
 
-		await interaction.reply(`Work done, added 2 Coronas to your balance. Current balance: ${userJson.bal} Coronas`);
+        // let jsonString = JSON.stringify(userJson);
+
+        // // Probably want to async this at some point
+        // fs.writeFileSync(`./data/users/${interaction.user.id}.json`, jsonString);
+
+		await interaction.reply(`Work done, added 2 Coronas to your balance. Current balance: ${newBal} Coronas`);
 	},
 };
