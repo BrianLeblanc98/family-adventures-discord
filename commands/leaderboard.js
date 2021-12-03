06fac240cac96b864b527e10fa69dc9e25aa7e4c
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 01:45:19 -0500

    test embeds in /leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,10 +8,29 @@ module.exports = {
 	async execute(interaction) {
         let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ 'bal': -1 }).toArray();
         
-        let output = "Current leaderboard";
+        let output = "Current leaderboard\n";
         for (user of result) {
             output += `${user.name}: ${user.bal}\n`
         }
-		await interaction.reply(output);
+		//await interaction.reply(output);
+
+        const exampleEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addField('Inline field title', 'Some value here', true)
+            .setImage('https://i.imgur.com/AfFp7pu.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+        await interaction.reply(exampleEmbed);
 	},
 };
