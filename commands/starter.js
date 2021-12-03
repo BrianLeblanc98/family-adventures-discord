31efec2c7fc1b9b6c74a804088905685c30c56e2
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 03:29:21 -0500

    edit stater command

--- commands/starter.js
+++ commands/starter.js
@@ -40,9 +40,10 @@ module.exports = {
         let message = await interaction.fetchReply();
         const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', time: 15000 });
 
-        collector.on('collect', i => {
+        collector.on('collect', async i => {
             if (i.user.id === interaction.user.id) {
-                i.followUp(`${i.user.id} clicked on the ${i.customId} button.`);
+                await i.deleteReply();
+                i.followUp(`${i.user.id} chose the ${i.label} as their starter car, welcome them to the family!`);
             } else {
                 i.followUp({ content: `These buttons aren't for you!`, ephemeral: true });
             }
