e15d256f401616fc3b666011aefb729fe311f792
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 01:32:37 -0500

    add leaderboard command fix

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,10 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('')
-		.setDescription(''),
+		.setName('leaderboard')
+		.setDescription('Show the family leaderboard'),
 	async execute(interaction) {
-        let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ bal: -1 });
+        let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ 'bal': -1 });
         console.log(result);
 		await interaction.reply('');
 	},
