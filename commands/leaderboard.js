96dc56f0da115dc6dd0ff0e0151264c663bf3d7f
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 01:29:50 -0500

    add leaderboard command fix

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('')
 		.setDescription(''),
 	async execute(interaction) {
-        let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ 'bal': -1 });
+        let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ bal: -1 });
         console.log(result);
 		await interaction.reply('');
 	},
