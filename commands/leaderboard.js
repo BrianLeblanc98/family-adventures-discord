eaeb8c688084452f773b3a09161899a300963ab0
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 01:28:37 -0500

    add leaderboard command

---
+++ commands/leaderboard.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('')
+		.setDescription(''),
+	async execute(interaction) {
+        let result = await mongoClient.db('familyAdventuresDiscordDb').collection('users').find().sort({ 'bal': -1 });
+        console.log(result);
+		await interaction.reply('');
+	},
+};
