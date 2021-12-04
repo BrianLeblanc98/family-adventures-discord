aac0dd7e88164cd5bb431d4c28654da49f0750f0
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-04 16:52:07 -0500

    add medium-race using new style

--- commands/medium-race.js
+++ commands/medium-race.js
@@ -1,70 +1,73 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const db = require('../util/db.js');
+const replys = require('../util/replys.js');
+const income = require('../util/income.json');
+
+const NAME = 'medium-race';
+const DESCRIPTION = 'Make our family proud, and take out some poser who thinks their car is fast.';
+const OPTION_DESCRIPTION = `Bet ${income[NAME].minBet}-${income[NAME].maxBet} Coronas, you\'ll get ${income[NAME].payoutPercent * 100}% back if you win.`;
 
 module.exports = {
+    name: NAME,
+    description: DESCRIPTION,
 	data: new SlashCommandBuilder()
-		.setName('medium-race')
-		.setDescription('Make our family proud, and take out some kid with a lightly tuned car.')
+		.setName(NAME)
+		.setDescription(DESCRIPTION)
         .addStringOption(option => 
             option.setName('bet')
-                .setDescription('How much you want to bet, you\'ll get 50% of what you bet as winnings. Between 100-2500')
+                .setDescription(OPTION_DESCRIPTION)
                 .setRequired(true)),
 	async execute(interaction) {
-        let userQuery = {'id': interaction.user.id.toString()};
-        let userData = await mongoClient.db('familyAdventuresDiscordDb').collection('users').findOne(userQuery);
-        if (!userData){
-            await interaction.reply({ content: `You're not part of the family <@${interaction.user.id}>! Join us by using /join`, ephemeral: true });
+        let userData = await db.getUser(interaction.user.id.toString());
+        let userInFamily = await db.inFamily(userData);
+        let userBoughtStarter = await db.hasStarter(userData);
+
+        if (!userInFamily){
+            await interaction.reply(replys.notInFamily(interaction));
             return;
         }
 
-        if (!userData.bought_starter) {
-            await interaction.reply({ content: `You haven't chosen your starter car <@${interaction.user.id}>! Use /starter to get your first car from the family.`, ephemeral: true });
+        if (!userBoughtStarter) {
+            await interaction.reply(replys.notBoughtStarter(interaction));
             return;
         }
-
-        let minBet = 100;
-        let maxBet = 2500;
-        let payoutPercent = 0.50;
-        let winPercent = 0.75;
-
+        
         let betString = interaction.options.getString('bet');
         let bet = 0;
-        if (betString === 'all') {
+        if (betString === 'all' || betString == 'max') {
             bet = userData.bal;
         } else if (/^\+?\d+$/.test(betString)) {
             // Check if it's a positive integer
             bet = parseInt(betString);
         } else {
-            await interaction.reply({ content: "Please enter 'all' or a positive non-decimal number", ephemeral: true });
+            await interaction.reply(replys.invalidBet());
             return;
         }
 
-        if (bet < minBet) {
-            await interaction.reply({ content: `The minimum bet is ${minBet} Coronas`, ephemeral: true });
+        if (bet < income[NAME].minBet) {
+            await interaction.reply(replys.underMinBet(income[NAME].minBet));
             return;
         } else if (bet > userData.bal) {
-            await interaction.reply(`Your current balance is ${userData.bal}, don't try to lie to the family about how much you have!`);
+            await interaction.reply(replys.overBalBet(userData.bal, bet));
             return;
-        } else if (bet > maxBet) {
-            bet = maxBet;
+        } else if (bet > income[NAME].maxBet) {
+            bet = income[NAME].maxBet;
         }
 
-        let newBal;
-        let carQuery = { '_id': userData.current_car_id };
-        let carData = await mongoClient.db('familyAdventuresDiscordDb').collection('cars').findOne(carQuery);
-        let carName = `${carData.year} ${carData.manufacturer} ${carData.name}`;
-        if (Math.random() < winPercent) {
-            // WIN
-            let winnings = Math.ceil(bet * payoutPercent);
-            newBal = userData.bal + winnings;
+        let carData = await db.getCar(userData.current_car_id);
+        let carName = db.getFullCarName(carData);
 
-            await interaction.reply(`<@${interaction.user.id}> decimated some kid with their ${carName}! Congrats on their ${winnings} Corona win!`);
+        if (Math.random() < income[NAME].winPercent) {
+            // WIN
+            let winnings = Math.ceil(bet * income[NAME].payoutPercent);
+            let newBal = await db.addBal(userData, winnings);
+
+            await interaction.reply(replys.basicRaceWin(NAME, interaction, carName, bet, winnings, newBal));
         } else {
             // LOSE
-            newBal = userData.bal - bet;
-            await interaction.reply(`<@${interaction.user.id}> lost a ${bet} Corona bet in their ${carName}. It was a close race, but still disappointing.`);
-        }
+            let newBal = await db.removeBal(userData, bet);
 
-        let update = { $set: { 'bal': newBal } };
-        await mongoClient.db('familyAdventuresDiscordDb').collection('users').updateOne(userQuery, update);
+            await interaction.reply(replys.basicRaceLose(NAME, interaction, carName, bet, newBal));
+        }
 	},
 };
