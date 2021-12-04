3e8862b69b6520b00052c55459fd384b3db4d5eb
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 22:16:37 -0500

    udpate help.js

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,18 @@ module.exports = {
 		let help = [
 			{ name: '/help', value: 'Display help information for the family.' },
 			{ name: '/join', value: 'Join the family, get your first car, and get ready to race!' },
-			{ name: '/work', value: 'Do some small work for the family, and get a small amount of Corona in return.' },
-			{ name: '/race', value: 'Set an entry fee, and anyone can join this winner-takes-all family race!' },
-			{ name: '/easy-race', value: 'Make our family proud, and take out some poser who thinks their car is fast.' },
-			{ name: '/bal', value: 'Shows your cuurent balance of Coronas, the currency in this family.' },
-			{ name: '/leaderboard', value: 'Show the family leaderboard.' },
 			{ name: '/starter', value: 'Choose your starting car. Each new family member only gets to do this once!' },
+			{ name: '\u200B', value: '\u200B' },
+			{ name: '/work', value: 'Do some small work for the family, and get a small amount of Corona in return.' },
+			{ name: '/easy-race', value: 'Make our family proud, and take out some poser who thinks their car is fast.' },
+			{ name: '/medium-race', value: 'Make our family proud, and take out some kid with a lightly tuned car.' },
+			{ name: '\u200B', value: '\u200B' },
+			{ name: '/bal', value: 'Shows your cuurent balance of Coronas, the currency in this family.' },
 			{ name: '/shop', value: 'Look at and buy many things from the family shop, using Corona as a currency!' },
-			{ name: '/user', value: 'Show the family all your current data.' }
+			{ name: '/user', value: 'Show the family all your current data.' },
+			{ name: '/leaderboard', value: 'Show the family leaderboard.' },
+			{ name: '\u200B', value: '\u200B' },
+			{ name: '/race', value: 'Set an entry fee, and anyone can join this winner-takes-all family race!' }
 		];
 
 		const embed = new MessageEmbed()
