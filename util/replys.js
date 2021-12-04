a27ea99d967a0ffa8a7428152f3e818eae3a6717
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-04 16:59:51 -0500

    add medium-race replys

--- util/replys.js
+++ util/replys.js
@@ -18,13 +18,19 @@ module.exports = {
         return `Your current balance is ${bal} and you want to bet ${bet}? Don't try to lie to the family about how much you have!`;
     },
     basicRaceWin(raceName, interaction, carName, bet, winnings, newBal) {
+        let ending = `\nCongrats on their ${winnings} Corona win! They now have ${newBal} Corona.`;
         if (raceName == 'easy-race') {
-            return `<@${interaction.user.id}> won an easy race betting ${bet} Corona!\nThey clapped some cheeks in their ${carName}!\nCongrats on their ${winnings} Corona win! They now have ${newBal} Corona.`
+            return `<@${interaction.user.id}> won an easy race betting ${bet} Corona!\nThey clapped some cheeks in their ${carName}, showing that the family easily prevails!${ending}`
+        } else if (raceName == 'medium-race') {
+            return `<@${interaction.user.id}> won a medium race betting ${bet} Corona!\nThey destroyed some kid in their ${carName}, showing that the family can still fight!${ending}`
         }
     },
     basicRaceLose(raceName, interaction, carName, bet, newBal) {
+        let ending = `They lost their ${bet} Corona bet. They now have ${newBal} Corona.`;
         if (raceName == 'easy-race') {
-            return `<@${interaction.user.id}> lost an easy race betting ${bet} Corona.\nThey got clapped in their ${carName}.\nThey lost their ${bet} Corona bet, bringing shame on the family. They now have ${newBal} Corona.`
+            return `<@${interaction.user.id}> lost an easy race betting ${bet} Corona.\nThey got clapped in their ${carName}, bringing incredible shame on the family.${ending}`
+        } else if (raceName == 'medium-race') {
+            return `<@${interaction.user.id}> lost a medium race betting ${bet} Corona.\nThey got destroyed in their ${carName}, bringing shame on the family.${ending}`
         }
     }
 }
