5520e0bc1fbffc67393d1338f3922dfe8fba65ea
Brian LeBlanc <brleblanc98@gmail.com>
2021-12-03 20:49:44 -0500

    update script

--- util/add-starter-cars-to-db.js
+++ util/add-starter-cars-to-db.js
@@ -8,7 +8,7 @@ const cars = require('../data/store/cars/requests.json');
 async function run() {
     try {
         await mongoClient.connect();
-        for (car in cars.newCars) {
+        for (car of cars.newCars) {
             await mongoClient.db('familyAdventuresDiscordDb').collection('cars').insertOne(car);
         }
     } finally {
